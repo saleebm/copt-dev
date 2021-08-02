dd340b4139eab384096598ce6b36d95dfebf6495
Mina Saleeb <saleebmina@icloud.com>
2021-08-02 18:07:47 -0400

    chore(config): set up tooling

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
-module.exports = {
+// @ts-check
+
+/**
+ * //@type {import('next').NextConfig}
+ **/
+ const config = {
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
+  experimental: {
+    stats: true
+  },
+  eslint: {
+    dirs: ['pages', 'components', 'lib'] // Only run ESLint on these directories during production builds (next build)
+  }
 }
+
+module.exports = config
