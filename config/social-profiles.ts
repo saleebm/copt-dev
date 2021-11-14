514fbc8323c82968ee937cc52cedaa2eb7914c46
saleebm <saleebmina@copt.dev>
2021-11-14 15:06:10 -0500

    feat: add polywork icon

--- config/social-profiles.ts
+++ config/social-profiles.ts
@@ -3,5 +3,13 @@ const twitterProfile = process.env.NEXT_PUBLIC_TWITTER_PROFILE
 const githubProfile = process.env.NEXT_PUBLIC_GITHUB_PROFILE
 const linkedinProfile = process.env.NEXT_PUBLIC_LINKEDIN_PROFILE
 const instagramProfile = process.env.NEXT_PUBLIC_INSTAGRAM_PROFILE
+const polyworkProfile = process.env.NEXT_PUBLIC_POLYWORK_PROFILE
 
-export { facebookProfile, instagramProfile, linkedinProfile, twitterProfile, githubProfile }
+export {
+  facebookProfile,
+  polyworkProfile,
+  instagramProfile,
+  linkedinProfile,
+  twitterProfile,
+  githubProfile
+}
